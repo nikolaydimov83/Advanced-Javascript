b322d211e73ccdf997d7fd1a8f7a8996ba48e8ea
Nikolay Dimov <nikolay.dimov83@gmail.com>
2015-05-16 22:59:27 +0300

    Hide-Show feature of the API added

--- 03Homework_/CanvasGeometryApi.js
+++ 03Homework_/CanvasGeometryApi.js
@@ -1,89 +1,121 @@
 /**
  * Created by User on 15.5.2015 ã..
  */
-
-var context = context||{};
-
-(function(context){
-    function ProcessedForm(pointAx,pointAy,pointBx,pointCx,pointCy,radius,figureColor,width,heigth){
-        this._pointAx=parseFloat(document.getElementById("pointA._x").value);
-        this._pointAy=parseFloat(document.getElementById("pointA._y").value);
-
-        this._pointBx=parseFloat(document.getElementById("pointB._x").value);
-        this._pointBy=parseFloat(document.getElementById("pointB._y").value);
-
-        this._pointCx=parseFloat(document.getElementById("pointC._x").value);
-        this._pointCy=parseFloat(document.getElementById("pointC._y").value);
-
-        this._radius=parseFloat(document.getElementById("radius").value);
-        this._figureColor=parseFloat(document.getElementById("figureColor").value);
-
-        this._width=parseFloat(document.getElementById("width").value);
-        this._height=parseFloat(document.getElementById("height").value);
-
-    }
-
-    ProcessedForm.prototype={
-        toString: function () {
-
-        },
-        getPointAx: function(){
-            return this._pointAx;
-        },
-        setPointAx: function(pointAx){
-            this._pointAx=pointAx;
-            return this;
-        },
-        getPointAy: function(){
-            return this._pointAy;
-        },
-        setPointAy: function(pointAy){
-            this._pointAy=pointAy;
-            return this;
-        },
-        getPointBx: function(){
-            return this._pointBx;
-        },
-        setPointBx: function(pointBx){
-            this._pointBx=pointBx;
-            return this;
-        },
-        getPointBy: function(){
-            return this._pointBy;
-        },
-        setPointBy: function(pointBy){
-            this._pointBy=pointBy;
-            return this;
-        },
-        getPointCx: function(){
-            return this._pointCx;
-        },
-        setPointCx: function(pointCx){
-            this._pointBx=pointCx;
-            return this;
-        },
-        getPointCy: function(){
-            return this._pointCy;
-        },
-        setPointCy: function(pointCy){
-            this._pointCy=pointCy;
-            return this;
+function readFormInDocumentByID(formID){
+    var kvpairs = [];
+    var form = document.getElementById(formID);
+    for ( var i = 0; i < form.elements.length; i++ ) {
+        var e = form.elements[i];
+        /*if (e.type==="button"){
+         continue
+         }*/
+        if (e.id!==""){
+            var key=encodeURIComponent(e.id);
+            var value=encodeURIComponent(e.value);
+            kvpairs[key]=value;
         }
 
     }
-    context.Constructor=Constructor;
-}(context));
-
-function ProcessForm(){
-    var pointAx = parseFloat(document.getElementById("pointA._x").value);
-    var pointAy=parseFloat(document.getElementById("pointA._y").value);
-    var pointA = new shapes.Point(pointAx,pointAy);
-    var figureColor=document.getElementById("color").value;
-    var radius = parseFloat(document.getElementById("radius").value);
-    var figureToDraw=new shapes.Circle(pointA,figureColor,radius);
-    var canvas="canvas";
-    figureToDraw.draw(canvas);
+    return kvpairs
 }
 
+function ProcessForm(hiddenFields,form,arrayWithDefaultTypes){
+    var allFormValues = readFormInDocument();
+    var pointAx=parseFloat(document.getElementById("pointA._x").value);
+    var pointAy=parseFloat(document.getElementById("pointA._y").value);
 
-document.getElementById("-").onclick =ProcessForm;
+    var pointBx=parseFloat(document.getElementById("pointB._x").value);
+    var pointBy=parseFloat(document.getElementById("pointB._y").value);
+
+    var pointCx=parseFloat(document.getElementById("pointC._x").value);
+    var pointCy=parseFloat(document.getElementById("pointC._y").value);
+
+    var radius=parseFloat(document.getElementById("radius").value);
+    var figureColor=document.getElementById("color").value;
+
+    var width=parseFloat(document.getElementById("width").value);
+    var height=parseFloat(document.getElementById("height").value);
+
+    var figureType=document.getElementById("geometric-shape").value;
+
+    var form = new shapeElementsParser.ProcessedForm(pointAx,pointAy,pointBx,pointBy,pointCx,
+                                    pointCy,radius,figureColor,width,height);
+
+    function changeLabelVisibility(element) {
+        var labels = document.getElementsByTagName('label');
+        for(var i = 0; i < labels.length; i ++) {
+            var attr = labels[i].getAttribute('for'); //or labels[i].htmlFor
+            if(attr === element.id) {
+                if (labels[i].style.visibility === 'hidden'&&element.type!=='hidden'){
+                    labels[i].style.visibility = 'visible'
+                }
+                else{
+                    labels[i].style.visibility = 'hidden';
+                }
+                //or labels[i].style.display = 'none';
+            }
+        }
+
+    }
+    function defineHiddenFields(hiddenFields,form){
+
+        for ( var i = 0; i < form.elements.length; i++ ) {
+            var e = form.elements[i];
+            var countHiddenPass=0;
+            var countVisiblePass=0;
+            for (var j = 0; j < hiddenFields.length; j++) {
+
+                if (e.id===hiddenFields[j]&&countVisiblePass===0){
+                    e.type='hidden';
+                    changeLabelVisibility(e);
+                    countHiddenPass++;
+                }
+                else{
+                    if (e.type==='hidden'&&countHiddenPass===0){
+                        if (hiddenFields.indexOf(e.id)===-1){
+                            e.type='text';
+                            changeLabelVisibility(e);
+                            countVisiblePass++;
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+
+    }
+    var formAsArray = readFormInDocumentByID("form1");
+    switch(figureType) {
+        case "circle":
+            defineHiddenFields(["pointB._x","pointB._y","pointC._x","pointC._y","width","height"],form1);
+            break;
+        case "triangle":
+            defineHiddenFields(["width","height","radius"],form1)
+            break;
+        case "line":
+            defineHiddenFields(["pointC._x","pointC._y","width","height","radius"],form1)
+            break;
+        case "point":
+            defineHiddenFields(["pointC._x","pointC._y","pointB._x","pointB._y","pointC._x","pointC._y","width","height","radius"],form1)
+            break;
+        case "rectangle":
+            defineHiddenFields(["pointC._x","pointC._y","pointB._x","pointB._y","pointC._x","pointC._y","radius"],form1)
+            break;
+        case "segment":
+            defineHiddenFields(["pointC._x","pointC._y","width","height","radius"],form1)
+            break;
+        default:
+
+    }
+
+
+}
+var readForm;
+document.getElementById("-").onclick = function(){ readFormInDocumentByID("form1")};
+console.log(readForm);
+document.getElementById("geometric-shape").onchange=ProcessForm;
---
+++ 03Homework_/ProcessedForm.js
@@ -0,0 +1,103 @@
+/**
+ * Created by User on 16.5.2015 ã..
+ */
+
+var shapeElementsParser = shapeElementsParser||{};
+
+(function(context){
+    function ProcessedForm(pointAx,pointAy,pointBx,pointBy,pointCx,pointCy,radius,figureColor,width,height){
+        this._pointAx=pointAx;
+        this._pointAy=pointAy;
+
+        this._pointBx=pointBx;
+        this._pointBy=pointBy;
+
+        this._pointCx=pointCx;
+        this._pointCy=pointCy;
+
+        this._radius=radius;
+        this._figureColor=figureColor;
+
+        this._width=width;
+        this._height=height;
+
+    }
+
+    ProcessedForm.prototype={
+        toString: function () {
+
+        },
+        getPointAx: function(){
+            return this._pointAx;
+        },
+        setPointAx: function(pointAx){
+            this._pointAx=pointAx;
+            return this;
+        },
+        getPointAy: function(){
+            return this._pointAy;
+        },
+        setPointAy: function(pointAy){
+            this._pointAy=pointAy;
+            return this;
+        },
+        getPointBx: function(){
+            return this._pointBx;
+        },
+        setPointBx: function(pointBx){
+            this._pointBx=pointBx;
+            return this;
+        },
+        getPointBy: function(){
+            return this._pointBy;
+        },
+        setPointBy: function(pointBy){
+            this._pointBy=pointBy;
+            return this;
+        },
+        getPointCx: function(){
+            return this._pointCx;
+        },
+        setPointCx: function(pointCx){
+            this._pointBx=pointCx;
+            return this;
+        },
+        getPointCy: function(){
+            return this._pointCy;
+        },
+        setPointCy: function(pointCy){
+            this._pointCy=pointCy;
+            return this;
+        },
+        getRadius: function(){
+            return this._radius;
+        },
+        setRadius: function(radius){
+            this._radius=radius;
+            return this;
+        },
+        getFigureColor: function(){
+            return this._figureColor;
+        },
+        setFigureColor: function(figureColor){
+            this._figureColor=figureColor;
+            return this;
+        },
+        getWidth: function(){
+            return this._width;
+        },
+        setWidth: function(width){
+            this._width=width;
+            return this;
+        },
+        getHeight: function(){
+            return this._heigth;
+        },
+        setHeight: function(height){
+            this._height=height;
+            return this;
+        }
+
+    }
+    context.ProcessedForm=ProcessedForm;
+}(shapeElementsParser));
